4e6449648df39aaeddaae2a548cab8f4a7ca06a8
sazsu <149969207+sazsu@users.noreply.github.com>
2025-09-16 18:34:57 +0300

    feat(git): enable current line blaming

--- lua/conf/plugins/gitsigns.lua
+++ lua/conf/plugins/gitsigns.lua
@@ -2,6 +2,8 @@ return {
   'gitsigns.nvim',
   event = { 'DeferredUIEnter', },
   after = function (_)
-    require('gitsigns').setup()
+    require('gitsigns').setup({
+      current_line_blame = true,
+    })
   end,
 }
