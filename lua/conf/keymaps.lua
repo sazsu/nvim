ffd5e2947d5972f1f7cea5887f872a972f076844
sazsu <149969207+sazsu@users.noreply.github.com>
2025-10-04 18:13:30 +0300

    feat(keymaps): add keybinding to format current buffer using lsp

--- lua/conf/keymaps.lua
+++ lua/conf/keymaps.lua
@@ -1,12 +1,12 @@
 local ls = require('luasnip')
 local map = vim.keymap.set
 
-map('n', '<leader>d', function ()
+map('n', '<leader>d', function()
   local vl = not vim.diagnostic.config().virtual_lines
   vim.diagnostic.config({ virtual_lines = vl })
 end)
 
-map('n', '<leader>r', function ()
+map('n', '<leader>r', function()
   local comp_enabled
   if vim.b.completion == nil then
     comp_enabled = true
@@ -16,8 +16,9 @@ map('n', '<leader>r', function ()
   vim.b.completion = not comp_enabled
 end)
 
-map({ 'i' }, '<c-e>', function () ls.expand() end, { silent = true })
-map({ 'i' }, '<c-j>', function () ls.jump(1) end, { silent = true })
-map({ 'i' }, '<c-k>', function () ls.jump(-1) end, { silent = true })
-map({ "i", "s" }, "<C-K>", function() ls.jump(-1) end, { silent = true })
+map({ 'i' }, '<c-e>', function() ls.expand() end, { silent = true })
+map({ 'i' }, '<c-j>', function() ls.jump(1) end, { silent = true })
+map({ 'i' }, '<c-k>', function() ls.jump(-1) end, { silent = true })
+map({ 'i', 's' }, '<C-K>', function() ls.jump(-1) end, { silent = true })
 
+map({ 'n', }, '<leader>fl', function() vim.lsp.buf.format() end, { silent = true })
