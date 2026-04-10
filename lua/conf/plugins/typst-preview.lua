f771e06b3505e438ab799a4488b3989dcdb57799
sazsu <149969207+sazsu@users.noreply.github.com>
2026-04-10 19:42:47 +0300

    fix: use custom launch cmd so that typst-preview works with nushell
    nushell's `open` is not the same as macos's `Open`
    
    see https://www.nushell.sh/commands/docs/open.html
    
    this should use `Open` on macos with nushell and the default one
    provided by the plugin itself otherwise

--- lua/conf/plugins/typst-preview.lua
+++ lua/conf/plugins/typst-preview.lua
@@ -1,9 +1,24 @@
+local function getopen_cmd()
+  local utils = require('typst-preview.utils')
+
+  local shell = vim.uv.os_getenv('SHELL')
+  local shell_name = shell and shell:match('([^/]+)$')
+
+  local open_cmd = utils.open_cmd
+  if utils.is_macos() and shell_name == 'nu' then
+    open_cmd = 'Open %s'
+  end
+  return open_cmd
+end
+
+
 return {
   'typst-preview.nvim',
   ft = { 'typst' },
-  after = function (_)
+  after = function(_)
     require('typst-preview').setup({
       invert_colors = 'auto',
+      open_cmd = getopen_cmd(),
     })
   end,
 }
