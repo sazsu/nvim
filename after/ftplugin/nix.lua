1fdec539421ba94b9ef2e47623af7685b8208a09
sazsu <149969207+sazsu@users.noreply.github.com>
2026-06-22 16:48:49 +0300

    feat(nix): QoL improment (insert semicolon after equals sign)

--- after/ftplugin/nix.lua
+++ after/ftplugin/nix.lua
@@ -1,1 +1,14 @@
 vim.treesitter.start()
+
+if not vim.g.nix_npairs_enabled then
+  local npairs = require('nvim-autopairs')
+  local Rule = require('nvim-autopairs.rule')
+  vim.g.nix_npairs_enabled = true
+  npairs.add_rule(
+    Rule('= ', ';', 'nix')
+    :with_pair(function(opts)
+      local before = opts.line:sub(opts.col - 2, opts.col - 2)
+      return not before:match('[=<>!]')
+    end)
+  )
+end
